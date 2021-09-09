adf8a126fb94a5411d2992a2a041fd2eb5bbd19e
Marcelo Bueno <marcelo.f.bueno@hotmail.com>
2021-09-09 10:23:46 -0300

    Incluído projeto de Landing Page do curso b7web

---
+++ b7web/landing-page/assets/js/scripts.js
@@ -0,0 +1,49 @@
+function changeBanner(){
+
+    var section = document.getElementById('banner');
+
+    if (section.classList.contains('banner1'))
+    {
+        section.classList.remove('banner1');
+        section.classList.add('banner2');
+
+        var title1 = document.getElementById('banner_title1');
+        var desc1 = document.getElementById('banner_desc1');
+        var title2 = document.getElementById('banner_title2');
+        var desc2 = document.getElementById('banner_desc2');
+
+        title1.style.display = 'block';
+        desc1.style.display = 'block';
+
+        title2.style.display = 'none';
+        desc2.style.display = 'none';
+    } else 
+    {
+        section.classList.remove('banner2');
+        section.classList.add('banner1');
+
+        var title1 = document.getElementById('banner_title1');
+        var desc1 = document.getElementById('banner_desc1');
+        var title2 = document.getElementById('banner_title2');
+        var desc2 = document.getElementById('banner_desc2');
+
+        title2.style.display = 'block';
+        desc2.style.display = 'block';
+
+        title1.style.display = 'none';
+        desc1.style.display = 'none';
+    }
+
+}
+
+setInterval(function(){changeBanner()}, 20000);
+
+window.onload = function() {
+    document.querySelector('.menu_icon').addEventListener('click', function(){
+        if (document.querySelector(".menu_mobile").style.display == "none"){
+            document.querySelector(".menu_mobile").style.display = "block";
+        } else {
+            document.querySelector(".menu_mobile").style.display = "none";
+        }
+    });
+}
